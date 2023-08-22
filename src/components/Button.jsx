a86ffadf010f8c4b3ed66806cf9783570565d2e8
Khaledalabsi <khaled.o.absi@gmail.com>
2023-08-22 04:04:21 +0300

    /

---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+}) => {
+  return (
+    <button
+      className={`flex justify-center items-center text-black gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-yellow-300 text-black border-yellow-300"
+      } rounded-full ${fullWidth && "w-full"}`}
+    >
+      {label}
+
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt='arrow right icon'
+          className='ml-2 rounded-full bg-yellow-300 w-5 h-5'
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
